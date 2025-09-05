e1ae51a829e36bb0c111ab37c7b449f8da680f9b
rakashit <rakashitinfo@gmail.com>
2025-09-05 09:36:40 +0530

    Update index.d.ts

--- family-tracker-web/src/types/index.d.ts
+++ family-tracker-web/src/types/index.d.ts
@@ -1,8 +1,14 @@
 export type Member = {
   id: string;
   name: string;
-  shelter: string;
   latitude: number;
   longitude: number;
   safe: boolean;
+};
+
+export type Shelter = {
+  id: string;
+  name: string;
+  latitude: number;
+  longitude: number;
 };
